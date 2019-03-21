48cb9de8e09ab587d46cebbc3772d03701bf5a65
k3x4 <xkexagias@gmail.com>
2019-03-21 11:21:52 +0200

    duplicate image changes

--- js/admin.js
+++ js/admin.js
@@ -1,34 +1,25 @@
 (function($) {
     
-    var ids = [];
+    var elems = [];
     $('#widgets-right .widget[id*="media_image"]').each(function(){
-        /*ids.push(this.id + '|' + $('[data-property="attachment_id"]', this).val());*/
-        ids.push($('[data-property="attachment_id"]', this).val());
+        elems.push({
+            elemId: this.id,
+            attachId: $('[data-property="attachment_id"]', this).val()
+        })
     });
 
-    if(ids.length > 0){
-        ids = ids.join(',');
+    if(elems.length > 0){
         $.ajax({
             url: ajaxurl,
             type: 'POST',
             data: {
                 'action': 'k3x4_widgets_imgs',
-                'ids': ids,
-                /*'action':                   'query-attachments',
-                'post__in':                  ids,
-                'query[posts_per_page]':    -1,*/
+                'ids': elems,
             },
             success: function(response) {
                 var items = $.parseJSON(response);
-                var i;
-                var id;
-                var $item;
-                for (i = 0; i < items.length; i++) { 
-                    id = items[i].id;
-                    img = items[i].url;
-                    $item = $('.media-widget-instance-property[value="'+id+'"]').closest('#widgets-right .widget[id*="media_image"]');
-                    $item.find('.widget-top').css('height', '100px').prepend('<img src="'+img+'" width="100px" height="100px" style="display:inline-block;float:left;" />');
-                    $item.find('.widget-top .ui-sortable-handle').css('height', '100px');
+                for (var i = 0; i < items.length; i++) { 
+                    $('#'+items[i].elemId).find('.widget-top').prepend('<img class="inject-img" src="'+items[i].attachUrl+'" />');
                 }
             },
         });
